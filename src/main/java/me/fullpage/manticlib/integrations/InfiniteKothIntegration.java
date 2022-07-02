7281a5c519dbeaf8852a3af094702d781190fd47
oliver11443 <oliver11443@icloud.com>
2022-07-02 13:06:42 +0200

    lib fix

--- src/main/java/me/fullpage/manticlib/integrations/InfiniteKothIntegration.java
+++ src/main/java/me/fullpage/manticlib/integrations/InfiniteKothIntegration.java
@@ -1,21 +1,29 @@
 package me.fullpage.manticlib.integrations;
 
-import me.fullpage.infinitekoth.data.KothData;
+import me.fullpage.infinitekoth.api.InfiniteKothAPI;
 import me.fullpage.manticlib.integrations.manager.Integration;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class InfiniteKothIntegration extends Integration {
 
+    private InfiniteKothAPI infiniteKothAPI;
+
     public InfiniteKothIntegration() {
         super("InfiniteKoth");
-        this.addRequiredClass("me.fullpage.infinitekoth.InfiniteKoth");
+        this.addRequiredClass("me.fullpage.infinitekoth.api.InfiniteKothAPI");
+    }
+
+    @Override
+    public void onEnable() {
+        infiniteKothAPI = (InfiniteKothAPI) JavaPlugin.getProvidingPlugin(InfiniteKothAPI.class);
     }
 
     public String getCapper() {
-        return isActive() ? KothData.getInstance().getCapper() : "";
+        return isActive() ? infiniteKothAPI.getCapper() : "";
     }
 
     public long getCapMillis() {
-        return isActive() ? (long) KothData.getInstance().capTime * 1000 : 0L;
+        return isActive() ? infiniteKothAPI.getCapMillis() : 0L;
     }
 
 }
