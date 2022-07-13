372726c5511814acab7c4b5852ebc9e213a3dc5c
oliver11443 <oliver11443@icloud.com>
2022-07-13 22:18:47 +0100

    Added economy methods within mining economy integration

--- src/main/java/me/fullpage/manticlib/integrations/MiningEconomyIntegration.java
+++ src/main/java/me/fullpage/manticlib/integrations/MiningEconomyIntegration.java
@@ -2,7 +2,12 @@ package me.fullpage.manticlib.integrations;
 
 import me.fullpage.manticlib.integrations.manager.Integration;
 import me.fullpage.miningeconomy.api.MiningEconomy;
+import me.fullpage.miningeconomy.api.interfaces.Economy;
+import me.fullpage.miningeconomy.api.interfaces.MPlayer;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public class MiningEconomyIntegration extends Integration {
 
@@ -20,6 +25,51 @@ public class MiningEconomyIntegration extends Integration {
 
     public MiningEconomy getMiningEconomy() {
         return miningEconomy;
+    }
+
+    public boolean hasEnough(@NotNull UUID uuid, long amount) {
+        if (!isActive()) return false;
+
+        MPlayer mPlayer = this.getMiningEconomy().getMPlayer(uuid.toString());
+        if (mPlayer == null) {
+            return false;
+        }
+
+        Economy economy = mPlayer.getPlayerEconomy();
+        return economy != null && economy.hasEnoughBalance(amount);
+    }
+
+    public boolean takeMoney(@NotNull UUID uuid, long amount) {
+        if (!isActive()) return false;
+
+        MPlayer mPlayer = this.getMiningEconomy().getMPlayer(uuid.toString());
+        if (mPlayer == null) {
+            return false;
+        }
+
+        Economy economy = mPlayer.getPlayerEconomy();
+        if (economy == null) {
+            return false;
+        }
+
+        economy.removeBalance(amount);
+        return true;
+    }
+    public boolean giveMoney(@NotNull UUID uuid, long amount) {
+        if (!isActive()) return false;
+
+        MPlayer mPlayer = this.getMiningEconomy().getMPlayer(uuid.toString());
+        if (mPlayer == null) {
+            return false;
+        }
+
+        Economy economy = mPlayer.getPlayerEconomy();
+        if (economy == null) {
+            return false;
+        }
+
+        economy.addBalance(amount);
+        return true;
     }
 
 }
