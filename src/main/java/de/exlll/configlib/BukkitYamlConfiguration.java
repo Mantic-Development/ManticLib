129984f97d2b18e3385bdab369409b192201fe0e
oliver11443 <oliver11443@icloud.com>
2022-07-24 14:08:52 +0100

    easier use of BukkitYamlConfiguration

--- src/main/java/de/exlll/configlib/BukkitYamlConfiguration.java
+++ src/main/java/de/exlll/configlib/BukkitYamlConfiguration.java
@@ -3,7 +3,10 @@ package de.exlll.configlib;
 import de.exlll.configlib.yaml.YamlConfiguration;
 import org.bukkit.configuration.file.YamlConstructor;
 import org.bukkit.configuration.file.YamlRepresenter;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.nio.file.Path;
 
 /**
@@ -11,12 +14,22 @@ import java.nio.file.Path;
  * {@code YamlConfiguration} that uses better default values.
  */
 public abstract class BukkitYamlConfiguration extends YamlConfiguration {
-    protected BukkitYamlConfiguration(Path path, BukkitYamlProperties properties) {
+    protected BukkitYamlConfiguration(@NotNull Path path, @NotNull BukkitYamlProperties properties) {
         super(path, properties);
     }
+    protected BukkitYamlConfiguration(@NotNull File file, @NotNull BukkitYamlProperties properties) {
+        super(file.toPath(), properties);
+    }
 
-    protected BukkitYamlConfiguration(Path path) {
+    protected BukkitYamlConfiguration(@NotNull Path path) {
         this(path, BukkitYamlProperties.DEFAULT);
+    }
+    protected BukkitYamlConfiguration(@NotNull File file) {
+        this(file.toPath(), BukkitYamlProperties.DEFAULT);
+    }
+
+    protected BukkitYamlConfiguration(@NotNull Plugin plugin, @NotNull String name) {
+        this(new File(plugin.getDataFolder(), name.replace(".yml", "").replace(".json", "").replaceAll("[^a-zA-Z\\d]", "_") + ".yml"), BukkitYamlProperties.DEFAULT);
     }
 
     public static class BukkitYamlProperties extends YamlProperties {
