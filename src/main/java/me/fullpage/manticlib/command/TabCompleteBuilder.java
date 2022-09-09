cd88982d4ed76986869100ef84b3c8a9f3b04527
Fullpage12 <74616919+Fullpage12@users.noreply.github.com>
2022-09-09 14:41:10 +0100

    Tab complete additions

--- src/main/java/me/fullpage/manticlib/command/TabCompleteBuilder.java
+++ src/main/java/me/fullpage/manticlib/command/TabCompleteBuilder.java
@@ -1,18 +1,39 @@
 package me.fullpage.manticlib.command;
 
+import org.bukkit.command.CommandSender;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class TabCompleteBuilder {
 
+    public static TabCompleteBuilder create(String[] args, CommandSender sender) {
+        return new TabCompleteBuilder(args, sender);
+    }
+
+    /**
+     * @see #create(String[], CommandSender)
+     */
+    @Deprecated
     public static TabCompleteBuilder create(String[] args) {
         return new TabCompleteBuilder(args);
     }
 
     private String[] args;
+    private CommandSender sender;
     private TabCompleteElement[] elements;
+
+    /**
+     * @see TabCompleteBuilder#create(String[], CommandSender)
+     */
+    @Deprecated
     private TabCompleteBuilder(String[] args) {
         this.args = args;
+        this.elements = new TabCompleteElement[0];
+    }
+    private TabCompleteBuilder(String[] args, CommandSender sender) {
+        this.args = args;
+        this.sender = sender;
         this.elements = new TabCompleteElement[0];
     }
 
@@ -27,7 +48,7 @@ public class TabCompleteBuilder {
     public List<String> build() {
         List<String> results = new ArrayList<>();
         for (TabCompleteElement element : this.elements) {
-            if (element.getIndex() == this.args.length-1) {
+            if (element.hasPermission(sender) && element.getIndex() == this.args.length-1) {
                 String arg = this.args[element.getIndex()].toLowerCase();
                 for (String result : element.getResults()) {
                     if (result!= null && result.toLowerCase().startsWith(arg)) {
--- src/main/java/me/fullpage/manticlib/command/TabCompleteElement.java
+++ src/main/java/me/fullpage/manticlib/command/TabCompleteElement.java
@@ -1,16 +1,52 @@
 package me.fullpage.manticlib.command;
 
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
 
 @Getter
 public class TabCompleteElement {
 
     private final int index;
     private final String[] results;
+    private String permission;
 
     public TabCompleteElement(int index, String... results) {
         this.index = index;
         this.results = results;
+    }
+
+    public TabCompleteElement setPermission(String permission) {
+        this.permission = permission;
+        return this;
+    }
+
+    public boolean hasPermission(CommandSender sender) {
+        return this.permission == null || sender == null || sender.hasPermission(this.permission);
+    }
+
+    public static TabCompleteElement getOnlinePlayers(int index, CommandSender sender) {
+        if (sender instanceof Player) {
+            return new TabCompleteElement(index, Bukkit.getOnlinePlayers().stream().filter(s -> ((Player) sender).canSee(s)).map(HumanEntity::getName).toArray(String[]::new));
+        }
+        return new TabCompleteElement(index, Bukkit.getOnlinePlayers().stream().map(HumanEntity::getName).toArray(String[]::new));
+    }
+
+    public static TabCompleteElement getAllPlayers(int index, CommandSender sender) {
+        if (sender instanceof Player) {
+            return new TabCompleteElement(index, Arrays.stream(Bukkit.getOfflinePlayers()).filter(s -> {
+                if (s instanceof Player) {
+                    return ((Player) sender).canSee((Player) s);
+                }
+                return true;
+            }).map(OfflinePlayer::getName).toArray(String[]::new));
+        }
+        return new TabCompleteElement(index, Arrays.stream(Bukkit.getOfflinePlayers()).map(OfflinePlayer::getName).toArray(String[]::new));
     }
 
 
