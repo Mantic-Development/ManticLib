c4fd7ae6051ef722838d62476411b55aca5de5dd
Fullpage12 <74616919+Fullpage12@users.noreply.github.com>
2022-10-19 12:53:38 +0100

    Added enchant libraries

--- src/main/java/me/fullpage/manticlib/ManticPlugin.java
+++ src/main/java/me/fullpage/manticlib/ManticPlugin.java
@@ -1,10 +1,14 @@
 package me.fullpage.manticlib;
 
+import com.google.common.annotations.Beta;
 import lombok.Getter;
 import me.fullpage.manticlib.command.ManticCommand;
+import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.Set;
 
 @Getter
@@ -33,6 +37,33 @@ public class ManticPlugin extends JavaPlugin {
     }
 
     public void onInnerDisable() {
+    }
+
+    private static final HashMap<String, String> providingPluginData = new HashMap<>();
+
+    @Beta
+    @NotNull
+    public static JavaPlugin getProvidingPlugin(@NotNull Class<?> clazz) {
+        JavaPlugin plugin = null;
+        try {
+            plugin = JavaPlugin.getProvidingPlugin(clazz);
+            if (!providingPluginData.containsKey(clazz.getName())) {
+                providingPluginData.put(clazz.getName(), plugin.getName());
+            }
+        } catch (IllegalStateException e) {
+            try {
+                String name = providingPluginData.get(clazz.getName());
+                if (name != null) {
+                    plugin = (JavaPlugin) Bukkit.getPluginManager().getPlugin(name);
+                }
+            } catch (Exception ex) {
+               plugin = null;
+            }
+        }
+        if (plugin == null) {
+            throw new IllegalStateException("\033[1;31mPlease do not use plugins like \"Plugman\" to load or unload a plugin during runtime. Instead use built-in reload commands in plugins or restart where possible.", e);
+        }
+        return plugin;
     }
 
 
