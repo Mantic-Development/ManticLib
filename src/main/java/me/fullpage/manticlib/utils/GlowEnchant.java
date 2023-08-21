1b097350bc6ad980ee3c7d786173c9be1f1dde2a
Fullpage12 <74616919+Fullpage12@users.noreply.github.com>
2023-08-21 15:48:00 +0100

    glow enchant

---
+++ src/main/java/me/fullpage/manticlib/utils/GlowEnchant.java
@@ -0,0 +1,71 @@
+package me.fullpage.manticlib.utils;
+
+import me.fullpage.manticlib.ManticLib;
+import me.fullpage.nmslib.EnchantInfo;
+import me.fullpage.nmslib.plugin.NMSLib;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+
+public final class GlowEnchant extends EnchantInfo {
+    private static GlowEnchant i;
+    private static Enchantment glow;
+
+    private GlowEnchant() {
+        super("Glow", 9999);
+        i = this;
+    }
+
+    public static Enchantment get() {
+        if (glow != null) return glow;
+        if (i == null) new GlowEnchant();
+
+        Enchantment enchantment = NMSLib.getNmsHandler().lookupEnchantment(i.getName(), i.getInternalId());
+        if (enchantment != null) return glow = enchantment;
+
+
+        try {
+            glow = NMSLib.getNmsHandler().buildEnchantment(i, ManticLib.get());
+            NMSLib.getNmsHandler().registerEnchantment(glow);
+            return glow;
+        } catch (Throwable t) {
+            glow = null;
+            throw new RuntimeException("Failed to register glow enchantment", t);
+        }
+    }
+
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    public int getStartLevel() {
+        return 1;
+    }
+
+    public EnchantmentTarget getItemTarget() {
+        return null;
+    }
+
+    public boolean canEnchantItem(ItemStack item) {
+        return true;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+    @Override
+    public boolean isCursed() {
+        return false;
+    }
+
+    public String getName() {
+        return "Glow";
+    }
+
+    public boolean conflictsWith(Enchantment other) {
+        return false;
+    }
+
+}
